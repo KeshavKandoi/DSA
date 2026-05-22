5677662d4ecc1d62283014e0cb72c4c09ac7d1a3
keshav kandoi <kandoikeshav2004@gmail.com>
2026-05-22 23:32:28 +0530

    String to Integer

---
+++ 19-StringPractice/String13.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cctype>
+
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i = 0;
+        int sign = 1;
+        long long result = 0;
+
+        // Skip leading spaces
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+
+        // Handle sign
+        if (i < n && s[i] == '-') {
+            sign = -1;
+            i++;
+        } 
+        else if (i < n && s[i] == '+') {
+            i++;
+        }
+
+        // Convert digits
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            // Overflow check
+            if (result > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return sign * result;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string s;
+    getline(cin, s);
+
+    cout << obj.myAtoi(s) << endl;
+
+    return 0;
+}
