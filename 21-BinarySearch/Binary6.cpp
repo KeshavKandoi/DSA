517a11fdfd88fa997bda088601d1d13f88d33c71
keshav kandoi <kandoikeshav2004@gmail.com>
2026-05-29 18:26:32 +0530

    Find First and Last Position

---
+++ 21-BinarySearch/Binary6.cpp
@@ -0,0 +1,94 @@
+// 34. Find First and Last Position of Element in Sorted Array
+
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+
+    int firstOccurrence(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int lastOccurrence(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = -1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {
+            firstOccurrence(nums, target),
+            lastOccurrence(nums, target)
+        };
+      }
+    
+      int main(){
+      int n;
+      cout<<"Enter the value of n:";
+      cin>>n;
+     
+
+      int target;
+      cout<<"Enter the value of target"<<endl;
+      cin>>target;
+
+       vector<int>kk(n);
+
+      cout<<"Enter the value of array"<<endl;
+
+
+      for(int i=0;i<n;i++){
+        cin>>kk[i];
+
+      }
+
+    cout<<"The array value is"<<endl;
+
+          for(int i=0;i<n;i++){
+            cout<<kk[i]<<" ";
+
+          }
+          cout<<endl;
+   
+      
+          vector<int>nums=searchRange(kk,target);
+          cout << "First Position = " << nums[0] << endl;
+         cout << "Last Position = " << nums[1] << endl;
+          
+
+    }
