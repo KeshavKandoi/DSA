521cc14a04cc32830dee73473facdc1aa672facc
keshav kandoi <kandoikeshav2004@gmail.com>
2026-06-18 23:42:42 +0530

    add implementation for matrix rotation

---
+++ 18-PRACTICE/Array26.cpp
@@ -0,0 +1,46 @@
+// Rotate image
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+
+    // Transpose
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // Reverse each row
+    for (int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    rotate(matrix);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
