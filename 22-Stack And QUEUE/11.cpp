d53f688dfcb957632bb08c578a71ebe7eb3716e9
keshav kandoi <kandoikeshav2004@gmail.com>
2026-07-13 05:09:14 +0530

    add implementation for post-fix to prefix conversion

---
+++ 22-Stack And QUEUE/11.cpp
@@ -0,0 +1,44 @@
+// postToPrefix
+
+#include<iostream>
+#include<stack>
+#include <algorithm>
+
+using namespace std;
+    string postToPre(string s) {
+        int n=s.size();
+        int i=0;
+          stack<string>st;
+
+
+        while(i<n){
+             if(s[i]>='A'&&s[i]<='Z'||s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9'){
+                st.push(string(1,s[i]));
+            }
+            else{
+                    
+            string t1=st.top();
+            st.pop();
+           string t2=st.top();
+            st.pop();
+
+            st.push(string(1,s[i])+(t2)+(t1));
+            }
+            i++;
+        }
+             return st.top();
+        }
+
+int main() {
+    string s;
+
+    cout << "Enter the string: ";
+    cin >> s;
+
+     cout << postToPre(s);
+
+    return 0;
+
+
+}
+
