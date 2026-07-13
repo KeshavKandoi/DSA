1afadfde631794cd3ced3419d85a750d14ed7af0
keshav kandoi <kandoikeshav2004@gmail.com>
2026-07-14 02:10:07 +0530

    refactor Trapping Rain Water implementation to use two-pointer approach

--- 22-Stack And QUEUE/15.cpp
+++ 22-Stack And QUEUE/15.cpp
@@ -1,38 +1,94 @@
 // 42. Trapping Rain Water
 
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int trap(vector<int>& height) {
+
+//     int n = height.size();
+
+//     vector<int> prefix(n);
+//     vector<int> suffix(n);
+
+
+//     prefix[0] = height[0];
+
+//     for (int i = 1; i < n; i++) {
+//         prefix[i] = max(prefix[i - 1], height[i]);
+//     }
+
+    
+//     suffix[n - 1] = height[n - 1];
+
+//     for (int i = n - 2; i >= 0; i--) {
+//         suffix[i] = max(suffix[i + 1], height[i]);
+//     }
+
+//     int water = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         water += min(prefix[i], suffix[i]) - height[i];
+//     }
+
+//     return water;
+// }
+
+// int main() {
+
+//     int n;
+
+//     cout << "Enter the size of array: ";
+//     cin >> n;
+
+//     vector<int> height(n);
+
+//     cout << "Enter the elements:\n";
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> height[i];
+//     }
+
+//     cout << "Water trapped = " << trap(height) << endl;
+
+//     return 0;
+// }
+
+
+
+// optimal
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
+    int trap(vector<int>& height) {
 
-int trap(vector<int>& height) {
+        int n=height.size();
+        int left=0;
+        int right=n-1;
+        int leftmax=0;
+        int rightmax=0;
+        int water=0;
 
-    int n = height.size();
+        while(left<right){
+            leftmax=max(leftmax,height[left]);
+            rightmax=max(rightmax,height[right]);
 
-    vector<int> prefix(n);
-    vector<int> suffix(n);
+            if(leftmax<rightmax){
+                water+=leftmax-height[left];
+                left++;
 
-    // Build Prefix Array
-    prefix[0] = height[0];
+            }
+            else{
+                 water+=rightmax-height[right];
+                 right--;
+            }
 
-    for (int i = 1; i < n; i++) {
-        prefix[i] = max(prefix[i - 1], height[i]);
+        }
+        return water;
+        
     }
-
-    // Build Suffix Array
-    suffix[n - 1] = height[n - 1];
-
-    for (int i = n - 2; i >= 0; i--) {
-        suffix[i] = max(suffix[i + 1], height[i]);
-    }
-
-    int water = 0;
-
-    for (int i = 0; i < n; i++) {
-        water += min(prefix[i], suffix[i]) - height[i];
-    }
-
-    return water;
-}
 
 int main() {
 
